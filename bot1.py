0b7e89c1aaede1d68ce4622af9aea35ea8dda125
Stanisław Piechota <s.piecho09@gmail.com>
2020-11-27 17:48:09 +0100

    v2.4.1 (photos, fixed covid, phrases)

--- bot1.py
+++ bot1.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 from ring import Ring
 from trans import Trans
 from perms import Perms
+from photos import Photos
 import datetime
 import aiohttp
 
@@ -24,11 +25,15 @@ async def on_ready():
 
 @bot.command(brief='Displays changelog')
 async def changelog(ctx):
-    file = discord.File("changelog.txt")
+    file = discord.File("additional/changelog.txt")
     await ctx.send("Changelog **v2.2.0**", file=file)
 
 @bot.command()
 async def covid(ctx):
+    if ctx.guild.id == 690261332003651621:
+        if ctx.author.id != 723448017151197196:
+            await ctx.send('This command is only available for **16 Adrian Lipka**')
+            return
     async with ctx.typing():
         async with aiohttp.ClientSession() as cs:
             async with cs.get('https://koronawirus.abczdrowie.pl/#koronawirus-w-polsce') as r:
@@ -38,14 +43,19 @@ async def covid(ctx):
         new_infections = resp[last-10:last].split('+')[1]
 
         last -= 13
-        infections = resp[last-10:last].split('>')[1]
+        infections = resp[last-10:last+10].split('>')[1].split('<')[0]
 
         embed = discord.Embed(title='COVID statistics', color=0xef3f2d)
         embed.add_field(name="New infections", value=new_infections, inline=True)
         embed.add_field(name="Infections", value=infections, inline=True)
         await ctx.send(embed=embed)
 
+@bot.command()
+async def link(ctx):
+    await ctx.send('https://discord.com/api/oauth2/authorize?client_id=773874155807834113&permissions=8&scope=bot')
+
 bot.add_cog(Ring(bot, data, dt))
 bot.add_cog(Trans(bot, dt))
 bot.add_cog(Perms(bot, data, dt))
+bot.add_cog(Photos(bot))
 bot.run(token1, reconnect=True)
